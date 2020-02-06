3bc4bff0c1a424fd85f06229efb6950965c6f437
Stewart <stewart.a.small@gmail.com>
2020-02-06 17:21:59 -0500

    Carousel class

---
+++ JCarousel.js
@@ -0,0 +1,70 @@
+class Carousel {
+
+	constructor(elements) {
+		this.offset = 5;
+		this.index = 0;
+		this.elements = elements;
+		this.queue = [];
+		this.indexMax = this.elements.length-1;
+
+		this.initialize();
+	}
+
+	initialize() {
+		let elemSize = this.elements.length;
+		let pos = 0;
+		for(let i = 0; i < this.offset; i++) {
+			if(i < 3){
+				this.queue.push(this.elements[i]);
+			} else {
+				elemSize -= 1; 
+				this.queue.unshift(this.elements[elemSize]);
+			}
+		}
+
+		this.index = this.elements.length - 2;
+	}
+
+	next() {
+		if(this.index >= this.indexMax){
+			this.index = 0;
+		} else {
+			this.index++;
+		}
+		this.generateQueue();
+		this.print();
+	}
+
+	prev() {
+		if(this.index <= 0){
+			this.index = this.indexMax;
+		} else {
+			this.index--;
+		}
+		this.generateQueue();
+		this.print();
+	}
+
+	generateQueue() {
+		this.queue = [];
+		this.recreateQueue(this.index);
+
+		let queueSize = this.offset - this.queue.length
+		if(queueSize !== 0){
+			this.recreateQueue(0);
+		}
+	}
+
+	recreateQueue(max) {
+		let elemSize = this.elements.length;
+		for(let i = max; i < elemSize; i++) {
+			if(this.queue.length < this.offset) {
+				this.queue.push(this.elements[i]);
+			}
+		}
+	}
+
+	print(){
+    	console.log(this.queue);
+    }
+}s
